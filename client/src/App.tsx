6f53af2abf838e3ae2567c826cc1581e42242665
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-19 13:52:04 -0400

    buildout bundling plugin.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,39 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
 
 const App: React.FC = () => {
+	const ref = useRef<any>(null);
 	const [ input, setInput ] = useState('');
 	// code is the transpiled output from esbuild.
 	const [ code, setCode ] = useState('');
 
 	const startService = async () => {
-		const service = await esbuild.startService({
+		ref.current = await esbuild.startService({
 			worker: true,
 			wasmURL: '/esbuild.wasm'
 		});
-
-		console.log(service);
 	};
 
 	useEffect(() => {
 		startService();
 	}, []);
 
-	const onClick = () => {
-		console.log(input);
+	const onClick = async () => {
+		if (!ref.current) {
+			return;
+		}
+
+		const result = await ref.current.build({
+			entryPoints: [ 'index.js' ],
+			bundle: true,
+			write: false,
+			plugins: [ unpkgPathPlugin() ]
+		});
+
+		console.log(result);
+
+		setCode(result.code);
 	};
 
 	return (
